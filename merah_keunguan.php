5ae3fed2620b8cc4285d511c0614f22a22da2790
SengkuniByte 🪽 <120631120+J13PhantomByte@users.noreply.github.com>
2026-01-16 13:23:34 +0700

    Versi Merah Keunguan

---
+++ merah_keunguan.php
@@ -0,0 +1,11 @@
+if($getsprice == "1000"){ $color = "#C2185B";}
+elseif($getsprice == "2000"){ $color = "#C2185B";}
+elseif($getsprice == "3000"){ $color = "#C2185B";}
+elseif($getsprice == "5000"){ $color = "#C2185B";}
+elseif($getsprice == "10000"){ $color = "#C2185B";}
+elseif($getsprice == "15000"){ $color = "#C2185B";}
+elseif($getsprice == "20000"){ $color = "#C2185B";}
+elseif($getsprice == "30000"){ $color = "#C2185B";}
+elseif($getsprice == "60000"){ $color = "#C2185B";}
+elseif($getsprice == "70000"){ $color = "#C2185B";}
+else{ $color = "#C2185B"; }
