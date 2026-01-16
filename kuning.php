b9775579dff1f677385425cf67b3f3a0801dd3ab
SengkuniByte 🪽 <120631120+J13PhantomByte@users.noreply.github.com>
2026-01-16 13:22:37 +0700

    versi kuning

---
+++ kuning.php
@@ -0,0 +1,11 @@
+if($getsprice == "1000"){ $color = "#FFC107";}
+elseif($getsprice == "2000"){ $color = "#FFC107";}
+elseif($getsprice == "3000"){ $color = "#FFC107";}
+elseif($getsprice == "5000"){ $color = "#FFC107";}
+elseif($getsprice == "10000"){ $color = "#FFC107";}
+elseif($getsprice == "15000"){ $color = "#FFC107";}
+elseif($getsprice == "20000"){ $color = "#FFC107";}
+elseif($getsprice == "30000"){ $color = "#FFC107";}
+elseif($getsprice == "60000"){ $color = "#FFC107";}
+elseif($getsprice == "70000"){ $color = "#FFC107";}
+else{ $color = "#FFC107"; }
