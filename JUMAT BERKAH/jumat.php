e5ed960e9aa048720fecfb0ec539a82fbf421cb3
SengkuniByte 🪽 <120631120+J13PhantomByte@users.noreply.github.com>
2026-02-02 21:28:26 +0700

    versi jumat

---
+++ JUMAT BERKAH/jumat.php
@@ -0,0 +1,11 @@
+if($getsprice == "1000"){ $color = "#FFFFFF";}
+elseif($getsprice == "2000"){ $color = "#FFFFFF";}
+elseif($getsprice == "3000"){ $color = "#FFFFFF";}
+elseif($getsprice == "5000"){ $color = "#FFFFFF";}
+elseif($getsprice == "10000"){ $color = "#FFFFFF";}
+elseif($getsprice == "15000"){ $color = "#FFFFFF";}
+elseif($getsprice == "20000"){ $color = "#FFFFFF";}
+elseif($getsprice == "30000"){ $color = "#FFFFFF";}
+elseif($getsprice == "60000"){ $color = "#FFFFFF";}
+elseif($getsprice == "70000"){ $color = "#FFFFFF";}
+else{ $color = "#000000"; }
